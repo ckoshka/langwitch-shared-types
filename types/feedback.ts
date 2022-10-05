95d9d2076f4745d7ae82bd9924abcadc7ba2d2d3
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-10-05 19:26:16 +1300

    removed feedback type

--- types/feedback.ts
+++
@@ -1,9 +0,0 @@
-import { MarkedResult } from "./context.ts";
-
-export type Feedback = {
-	result?: MarkedResult;
-	userAnswer: string;
-	known?: boolean; // should be a list of strings
-	terminate?: boolean;
-	// should also include hinting?
-};
